abccf01e3663a02d14ea08a40162d25baaad13d6
Seu Nome ou Usuario Do Github <seu-email@exemplo.com>
2026-08-07 07:23:19 -0300

    Fix: Criado o schema Guild.js para suporte ao prefixo

---
+++ database/schemas/Guild.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const GuildSchema = new mongoose.Schema({
+  guildId: { type: String, required: true, unique: true },
+  prefix: { type: String, default: '!' }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Guild', GuildSchema);
